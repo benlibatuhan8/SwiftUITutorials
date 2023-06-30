343eda094bcc4c97a5405277f3523318333af6c4
benlibatuhan8 <benlibatuhan6@gmail.com>
2023-06-30 19:37:29 +0300

    Initial Commit

---
+++ SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by Batuhan Benli on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
